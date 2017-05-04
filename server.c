e4b76eedbae7bff35410fed580d7714ebd126767
Daeya <yayah.miss9377@gmail.com>
2017-05-04 18:30:49 +0100

    Add files via upload

---
+++ server.c
@@ -0,0 +1,87 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 8080
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello client, i am server";
+    int length,c;
+    char *begin, *end, temp;
+      
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+      
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+      
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, 
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+                       (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    //read client
+    valread = read( new_socket , buffer, 1024);
+    printf("Client message: %s\n",buffer );
+    //sending server message
+    send(new_socket , hello , strlen(hello) , 0 );
+    
+    
+ char *bufferReverse;
+ bufferReverse = buffer;
+    
+    length = strlen(bufferReverse);
+    begin  = bufferReverse;
+    end    = bufferReverse;
+
+    for (c = 0; c < length - 1; c++)
+      end++;
+ 
+   for (c = 0; c < length/2; c++)
+   {        
+      temp   = *end;
+      *end   = *begin;
+      *begin = temp;
+ 
+      begin++;
+      end--;
+   }
+
+//send(new_socket , bufferReverse, strlen(bufferReverse), 1024);
+printf("Client message reverse:%s\n",bufferReverse );
+    
+    return 0;
+}
